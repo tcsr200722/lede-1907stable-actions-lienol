30ac8d9c3dd5813adbb9910cec83d0f4f13751ec
ledewrt <61609258+ledewrt@users.noreply.github.com>
2020-10-17 12:34:04 +0800

    Update diy.sh

--- devices/x86_64/diy.sh
+++ devices/x86_64/diy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-
-sed -i "s/+luci\( \|$\)//g"  package/*/*/*/Makefile
+# 修改主机名字
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='Lede-X64'' package/default-settings/files/zzz-default-settings
+#添加img编译时间前缀。
+sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(shell date +%Y%m%d)-LEDE-1907-x64-/g' include/image.mk
+# 修改内核版本
+#sed -i 's/KERNEL_PATCHVER:=4.14/KERNEL_PATCHVER:=4.19/g' target/linux/x86/Makefile
