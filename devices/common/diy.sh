6d53728e3d69c64c67d4cba10954e3b8bd18faf4
ledewrt <61609258+ledewrt@users.noreply.github.com>
2020-10-19 00:38:35 +0800

    Update diy.sh

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -30,7 +30,7 @@ sed -i 's/DEPENDS:=/DEPENDS:=+AdGuardHome /g' package/diy/luci-app-adguardhome/M
 #argon主题
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/diy/luci-theme-argon
 #argon主题
-git clone -b master https://github.com/garypang13/luci-theme-edge.git  package/diy/luci-theme-edge
+git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git  package/diy/luci-theme-edge
 
 git clone https://github.com/ledewrt/luci-app-eqos.git  package/diy/luci-app-eqos
 git clone https://github.com/jefferymvp/luci-app-koolproxyR.git  package/diy/luci-app-koolproxyR
