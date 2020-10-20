d0e47c3e6d271b21c837c71b8e9eea012c1481c5
ledewrt <61609258+ledewrt@users.noreply.github.com>
2020-10-20 08:05:10 +0800

    Update diy.sh

--- devices/x86_64/diy.sh
+++ devices/x86_64/diy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 修改主机名字
-sed -i 's/hostname='OpenWrt'/hostname='LEDE_X64'/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/LEDE_X64/g' package/base-files/files/bin/config_generate
 #添加img编译时间前缀。
 sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(shell date +%Y%m%d)-LEDE-1907-X64-/g' include/image.mk
 # 修改内核版本
