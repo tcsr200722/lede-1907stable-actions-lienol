38b68b98c644aa30ceeaba266217e9674092bb21
ledewrt <61609258+ledewrt@users.noreply.github.com>
2020-11-10 07:17:40 +0800

    Update diy.sh

--- devices/x86_64/diy.sh
+++ devices/x86_64/diy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 修改主机名字
-sed -i 's/OpenWrt/LEDE/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/LedeWrt/g' package/base-files/files/bin/config_generate
 #添加img编译时间前缀。
-sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(shell date +%Y%m%d)-LEDE-1907-X64-/g' include/image.mk
+sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(shell date +%Y%m%d)-ledewrt-1907-X64-/g' include/image.mk
 # 修改内核版本
 #sed -i 's/KERNEL_PATCHVER:=4.14/KERNEL_PATCHVER:=4.19/g' target/linux/x86/Makefile
