3bb1b7ca3391aae2330685e332b3ae039be5e2db
ledewrt <61609258+ledewrt@users.noreply.github.com>
2020-12-02 23:57:21 +0800

    Update diy.sh

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -12,7 +12,6 @@
 #=================================================
 #添加固件版本描述。
 rm -Rf package/lean/luci-app-ssr-plus
-rm -Rf package/lean/luci-app-wrtbwmon
 #rm -Rf package/diy/OpenAppFilter
 rm -Rf package/diy/luci-app-adguardhome
 # 修改登陆地址
@@ -24,17 +23,16 @@ sed -i '/option filter_aaaa 1/d' package/network/services/dnsmasq/files/dhcp.con
 #sed -i 's/net.netfilter.nf_conntrack_max=65535/net.netfilter.nf_conntrack_max=105535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
 #添加自定义插件1。
 git clone https://github.com/vernesong/OpenClash.git package/diy/luci-app-openclash
-git clone https://github.com/garypang13/openwrt-adguardhome.git package/diy/openwrt-adguardhome
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git  package/diy/luci-app-adguardhome
-sed -i '/resolvfile=/d' package/diy/luci-app-adguardhome/root/etc/init.d/AdGuardHome
-sed -i 's/DEPENDS:=/DEPENDS:=+AdGuardHome /g' package/diy/luci-app-adguardhome/Makefile
+#git clone https://github.com/garypang13/openwrt-adguardhome.git package/diy/openwrt-adguardhome
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git  package/diy/luci-app-adguardhome
+#sed -i '/resolvfile=/d' package/diy/luci-app-adguardhome/root/etc/init.d/AdGuardHome
+#sed -i 's/DEPENDS:=/DEPENDS:=+AdGuardHome /g' package/diy/luci-app-adguardhome/Makefile
 #argon主题
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/diy/luci-theme-argon
 
 git clone https://github.com/ledewrt/luci-app-eqos.git package/diy/luci-app-eqos
 git clone https://github.com/ledewrt/luci-app-ledeproxy.git package/diy/luci-app-ledeproxy
 git clone https://github.com/tty228/luci-app-serverchan.git package/diy/luci-app-serverchan
-git clone https://github.com/brvphoenix/luci-app-wrtbwmon.git  package/diy/luci-app-wrtbwmon
 #git clone https://github.com/destan19/OpenAppFilter.git package/diy/OpenAppFilter
 #git clone https://github.com/fw876/helloworld.git package/diy/luci-app-ssr-plus
 #git clone https://github.com/docker/docker-ce.git package/diy/luci-app-docker-ce
